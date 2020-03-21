a66a17141f3630347188f2297497444727cd8e85
m-nithish <62443335+m-nithish@users.noreply.github.com>
2020-03-21 04:30:50 -0700

    Add files via upload

---
+++ A.2.PUBLISHER.py
@@ -0,0 +1,18 @@
+import rospy
+from std_msgs.msg import String
+
+def publisher1():
+    p1 = rospy.Publisher('welcome_message', String, queue_size=10)
+    rospy.init_node('publisher1', anonymous=True)
+    rate = rospy.Rate(5) 
+    while not rospy.is_shutdown():
+        str = "Welcome to Abhiyaan"
+        rospy.loginfo(str)
+        p1.publish(str)
+        rate.sleep()
+ 
+if __name__ == '__main__':
+    try:
+        publisher1()
+    except rospy.ROSInterruptException:
+        pass
